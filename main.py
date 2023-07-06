cb9ac6563db18600624920cc5e3d65a95c4239a3
Surya <123630128+theOne1100@users.noreply.github.com>
2023-07-06 16:50:18 +1200

    Update and rename PasswordSign+Check.py to main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import pyinputplus as pyip
-# import hashlib
 
 
 
-def signin():# from cryptography.fernet import Fernet
+def signin():
     userpass = open("1DatabasePy23//usernames_pass.txt","a+")
     print("Welcome to DatabasePy23")
     print("Here you can create an account and store your contacts")
@@ -23,25 +22,22 @@ def signin():# from cryptography.fernet import Fernet
             NewPassword = pyip.inputPassword("Please input your Password: ")
             NewPasswordVer = pyip.inputPassword("Please input your Password again: ")
             if NewPassword == NewPasswordVer:
-                print("Password verification Success")
-                print("Saving Now")
+                print("Password verification success")
+                print("Saving Now...")
                 passwordverSuccess == True
                 userpass.write(NewUsername +":"+NewPassword+ "\n")
                 userpass.close()
-                print("Closing the program now")
-                print("Please sign in with your new credentials on a new instance")
+                print("Closing the program now...")
+                print("Please sign in with your new credentials on a new instance.")
                 quit()
 
     elif start_choice.lower().strip() == "s":
         print("You have chosen to : Sign in with an existing account")
-        #Signing in with existing account
         signUser = pyip.inputStr("Please input your Username: ")
         signPass = pyip.inputPassword("Please input your Password: ")
         checksign = signUser + ":" + signPass
-        # print(checksign)
         userpass.seek(0)
         file_fullread = userpass.read()
-        # print(file_fullread)
         if checksign in file_fullread:
             print("Signed In!")
             print("What would you like to do today?")
@@ -49,21 +45,14 @@ def signin():# from cryptography.fernet import Fernet
             print("(c)reate new contact", "(s)how all contacts, (d)elete a contact or s(e)arch for a contact")
             print("Or you can choose (q) for quit")
             
-            # if cShow == "q":
-            #     print("Thank You")
-            #     print("Quitting Program Now!")
-            #     quit()
-            # elif cShow == "r":
         else:
             print("Sorry, the credentials that you have put in do not work")
             quit()
 
 
     elif start_choice.lower().strip() == "q":
-        print("You have chose to : Quit")
-        print("The program has been exited")
-        #Quitting the program
+        print("You have chose to: Quit")
+        print("The program is exiting...")
         quit()
-    # print("YAY")
     userpass.close()
 signin()
