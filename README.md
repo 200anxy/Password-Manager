862945ca207f4752d64887555745fc77732652cf
Surya <123630128+theOne1100@users.noreply.github.com>
2023-07-08 18:48:56 +1200

    Changed code and added default usernames and passwords.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PasswordManager
-This program uses PyInputPlus to get the users passwords, check them against another text file and signs you in. Also allows you to create new account and saves credentials automatically to textfile
+This program uses PyInputPlus to get the users passwords, check them against another text file and signs you in. It also saves credentials automatically to a textfile.
+
+The default username and password is:
+Username: John
+Password: Doe
+
+Thank you!
--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 import pyinputplus as pyip
+import hashlib
+
+signedIn = False
+signInAttempts = 0
+
+def shahash(string):
+    hash = hashlib.sha256(text.encode('UTF-8')).hexdigest()
+    return hash
 
 
 
 def signin():
     userpass = open("usernames_pass.txt","a+")
-    print("Welcome to DatabasePy23")
-    print("Here you can create an account and store your contacts")
-    print("Your credentials are then encrypted for safety")
-    print("Please choose (c) for creating a new account")
-    print("Please chooose (s) for signing in with an existing one")
-    print("And if you want to quit, choose (q)")
+    print('Starting password check... ')
+    print('You can either (C)reate a new account, (S)ign in with an existing account, or (Q)uit.')
 
     start_choice = pyip.inputChoice(["c","s","q"])
 
     if start_choice.lower().strip()  == "c":
-        print("You have chose to : Create a new account")
+        print("You have chose to: Create a new account.")
         NewUsername = pyip.inputStr("Please input your Username: ")
         #Creating new account
         passwordverSuccess = False
         while passwordverSuccess != True:
-            NewPassword = pyip.inputPassword("Please input your Password: ")
-            NewPasswordVer = pyip.inputPassword("Please input your Password again: ")
+            NewPassword = shahash(pyip.inputPassword("Please input your Password: "))
+            NewPasswordVer = shahash(pyip.inputPassword("Please input your Password again: "))
             if NewPassword == NewPasswordVer:
                 print("Password verification success")
                 print("Saving Now...")
@@ -28,26 +32,28 @@ def signin():
                 userpass.write(NewUsername +":"+NewPassword+ "\n")
                 userpass.close()
                 print("Closing the program now...")
-                print("Please sign in with your new credentials on a new instance.")
+                print("Please sign in with your new credentials on a new instance.\nThanks!")
                 quit()
 
     elif start_choice.lower().strip() == "s":
         print("You have chosen to : Sign in with an existing account")
-        signUser = pyip.inputStr("Please input your Username: ")
-        signPass = pyip.inputPassword("Please input your Password: ")
-        checksign = signUser + ":" + signPass
-        userpass.seek(0)
-        file_fullread = userpass.read()
-        if checksign in file_fullread:
-            print("Signed In!")
-            print("What would you like to do today?")
-            print("Please choose from:")
-            print("(c)reate new contact", "(s)how all contacts, (d)elete a contact or s(e)arch for a contact")
-            print("Or you can choose (q) for quit")
+        while signedIn == False and signInAttempts > 3:
+            signUser = pyip.inputStr("Please input your Username: ")
+            signPass = shahash(pyip.inputPassword("Please input your Password: "))
+            checksign = signUser + ":" + signPass
+            file_fullread = userpass.read()
+            if checksign in file_fullread:
+                print("Signed In!")
+                signedIn = True
+                return signedIn
             
-        else:
-            print("Sorry, the credentials that you have put in do not work")
-            quit()
+            else:
+                print("Sorry, the credentials that you have put in do not work.")
+                signInAttempts += 1
+                continue
+        print("You have logged in with incorrect credentials too many times. Please try again later.")
+        userpass.close()
+        exit()    
 
 
     elif start_choice.lower().strip() == "q":
@@ -55,4 +61,6 @@ def signin():
         print("The program is exiting...")
         quit()
     userpass.close()
+
 signin()
+
